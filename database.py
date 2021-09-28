0509db799bdbb3b51f2f8545f4e3cdaf40dfd0bc
Ivan S <ivan.leondager@gmail.com>
2021-09-28 19:24:49 -0400

    adding get method to visualize data when adding data

--- database.py
+++ database.py
@@ -10,12 +10,13 @@ class LogDatabase:
 
 
 class EyeDatabase:
-    table_by_session_id = {}
-    table_by_category = {}
-    table_by_time = []
+
 
     def __init__(self, logger):
         self.logger = logger
+        self.table_by_session_id = {}
+        self.table_by_category = {}
+        self.table_by_time = []
 
     @staticmethod
     def check_valid_date(data_list, new_element):
@@ -26,25 +27,35 @@ class EyeDatabase:
         return True
 
     def insert(self, data):
-
         mandatory_keys = ["session_id", "category", "name", "data", "timestamp"]
-        if data in mandatory_keys:
-            if data['session_id'] in self.table_by_session_id:
-                if self.check_valid_date(table_by_session_id['session_id'], data):
-                    self.table_by_session_id['session_id'].append(data)
+        valid = True
+        for key in mandatory_keys:
+            if key not in data.keys():
+                valid = False
+                break
+        if valid:
+            if data['session_id'] in self.table_by_session_id.keys():
+                if self.check_valid_date(self.table_by_session_id[data['session_id']], data):
+                    self.table_by_session_id[data['session_id']].append(data)
                 else:
                     self.logger.insert(
-                        {"process": "saving in database", "type": "Process", "message": "invalid timestamp"})
+                        {"process": "saving in database", "type": "Error", "message": "invalid timestamp"})
                     return
             else:
-                self.table_by_session_id['session_id'] = []
-                self.table_by_session_id['session_id'].append(data)
-            if data['category'] in self.table_by_category:
-                if self.check_valid_date(table_by_category['category'], data):
-                    self.table_by_category['category'].append(data)
+                self.table_by_session_id[data['session_id']] = []
+                self.table_by_session_id[data['session_id']].append(data)
+            if data['category'] in self.table_by_category.keys():
+                if self.check_valid_date(self.table_by_category[data['category']], data):
+                    self.table_by_category[data['category']].append(data)
+                else:
+                    self.logger.insert(
+                        {"process": "saving in database", "type": "Error", "message": "invalid timestamp"})
+                    return
             else:
-                self.table_by_category['category'] = []
-                self.table_by_category['category'].append(data)
+                self.table_by_category[data['category']] = []
+                self.table_by_category[data['category']].append(data)
             self.table_by_time.append(data)
+            self.logger.insert(
+                {"process": "saving in database", "type": "Process", "message": "saved successfully"})
         else:
             self.logger.insert({"process": "saving in database", "type": "Process", "message": "invalid data"})

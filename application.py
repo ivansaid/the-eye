8e67be0375935d65b821108a61841e156726a7f5
Ivan S <ivan.leondager@gmail.com>
2021-09-28 17:43:14 -0400

    adding logger and trusted applications

--- application.py
+++ application.py
@@ -1,25 +1,33 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from http.client import HTTPResponse
-from os import curdir, sep
+from database import LogDatabase, EyeDatabase
 
+logger = None
+eye_database = None
+valid_sessions = {'e2085be5-9137-4e4e-80b5-f1ffddc25423': True}
 
-# Create a index.html aside the code
-# Run: python server.py
-# After run, try http://localhost:8080/
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/the-eye':
-            request_handler(self)
+            if self.headers.get('session_id', False) and valid_sessions.get(self.headers.get('session_id')):
+                logger.insert(
+                    {"process": "processing request", "type": "Process", "message": "request proccess by user: " +
+                                                                                    self.headers.get(
+                                                                                        'session_id')})
+                self.send_response(200)
+                self.end_headers()
+            else:
+                logger.insert(
+                    {"process": "processing request", "type": "Error", "message": "unauthorized or invalid user"})
+                self.send_response(401)
+                self.end_headers()
 
         else:
             self.send_error(404, 'File not found!')
             self.end_headers()
 
-
-def request_handler(self):
-    self.send_response(200)
-    self.end_headers()
+    def request_handler(self):
+        pass
 
 
 def run():
@@ -34,4 +42,6 @@ def run():
 
 
 if __name__ == '__main__':
+    logger = LogDatabase()
+    eye_database = EyeDatabase(logger)
     run()
--- database.py
+++ database.py
@@ -26,13 +26,15 @@ class EyeDatabase:
         return True
 
     def insert(self, data):
+
         mandatory_keys = ["session_id", "category", "name", "data", "timestamp"]
         if data in mandatory_keys:
             if data['session_id'] in self.table_by_session_id:
                 if self.check_valid_date(table_by_session_id['session_id'], data):
                     self.table_by_session_id['session_id'].append(data)
                 else:
-                    self.logger.insert({"process": "saving in database", "error message": "invalid timestamp"})
+                    self.logger.insert(
+                        {"process": "saving in database", "type": "Process", "message": "invalid timestamp"})
                     return
             else:
                 self.table_by_session_id['session_id'] = []
@@ -45,4 +47,4 @@ class EyeDatabase:
                 self.table_by_category['category'].append(data)
             self.table_by_time.append(data)
         else:
-            self.logger.insert({"process": "saving in database", "error message": "invalid data"})
+            self.logger.insert({"process": "saving in database", "type": "Process", "message": "invalid data"})

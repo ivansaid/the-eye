b156fc7d94d0def0d2aaa55c2a23ee9cfa4ed5b9
Ivan S <ivan.leondager@gmail.com>
2021-09-28 19:24:49 -0400

    adding get method to visualize data when adding data, fixing insert errors, adding post method, adding thread to avoid requests hanging

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from database import LogDatabase, EyeDatabase
+from threading import Thread
+import json
 
 logger = None
 eye_database = None
@@ -8,12 +10,28 @@ valid_sessions = {'e2085be5-9137-4e4e-80b5-f1ffddc25423': True}
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        if self.path == '/the-eye-data':
+            if self.headers.get('user', False) and self.headers.get('user') == 'admin':
+                copy = eye_database.__dict__.copy()
+                del copy['logger']
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps(copy).encode(encoding='utf_8'))
+            else:
+                self.send_response(401)
+                self.end_headers()
+
+
+    def do_POST(self):
         if self.path == '/the-eye':
             if self.headers.get('session_id', False) and valid_sessions.get(self.headers.get('session_id')):
                 logger.insert(
                     {"process": "processing request", "type": "Process", "message": "request proccess by user: " +
                                                                                     self.headers.get(
                                                                                         'session_id')})
+
+                Thread(target=self.request_handler).start()
                 self.send_response(200)
                 self.end_headers()
             else:
@@ -27,7 +45,8 @@ class RequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
 
     def request_handler(self):
-        pass
+        body = json.loads(self.rfile.read(int(self.headers.get('content-length', 0))))
+        eye_database.insert(body)
 
 
 def run():
